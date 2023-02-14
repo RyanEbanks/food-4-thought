e88b26cd0705a670f3287552bc5d521598ab2672
JordAnne <jbethune37@gmail.com>
2023-02-13 19:27:01 -0500

    updated html for dessert and entrees. updated js for desserts and entrees

--- assets/desserts.js
+++ assets/desserts.js
@@ -2,23 +2,26 @@ var formEL = document.querySelector (".pure-form");
 var infoEL = document.querySelector ("#info");
 var showtextEL =document.querySelector (".show");
 var showinfoEL = document.querySelector (".show-info");
-
+var choicesEl = document.querySelector(".choices");
+​
+​
 var subButton = function(event) {
     event.preventDefault();
-
+​
     var infoInput = infoEL.value.trim();
     if(infoInput) {
         displayInfo(infoInput);
     } else {
         showinfoEL.innerHTML = "Nothing was Found"
     }
-
+​
 }
-
+​
 function displayInfo(search) {
-    var url = "https://api.edamam.com/api/recipes/v2?type=public&q="+search+"&app_id=730b99e5&app_key=2eacc20905ac41a9a0d49163a5a68fec&dishType=Desserts&dishType=Sweets"
-    console.log(url);
-    fetch(url) 
+    var dessertsUrl = "https://api.edamam.com/api/recipes/v2?type=public&q="+search+"&app_id=730b99e5&app_key=2eacc20905ac41a9a0d49163a5a68fec&dishType=Desserts&dishType=Sweets"
+    
+    console.log(dessertsUrl);
+    fetch(dessertsUrl) 
     .then(function(response){
         if(response.ok) {
             response.json().then(function(data) {
@@ -27,6 +30,7 @@ function displayInfo(search) {
                     showinfoEL.innerHTML = "Nothing was Found";
                     return;
                 }
+​
                 displayTxt(data.hits);
                 console.log(data.hits);
                 console.log("General Data" + data);
@@ -34,42 +38,43 @@ function displayInfo(search) {
         }
     });
 }
-
-
+​
+​
+//Function to loop through the recipe array and display the individual stuff
 function displayTxt(txt) {
     var foodContainerHtml = "";
+//You could add a line of code here to store calories and make it a whole number then represent it below
  for(var i = 0; i < txt.length; i++) {
     foodContainerHtml += `
     <div class="food-container food-${i}">
     <img src= "${txt[i].recipe.image}">
     <p>${txt[i].recipe.label}</p>
-    <p>Description<p>
-    <p>Description Text<p>
-    <p>Calories: ${txt[i].recipe.calories}Kcal</p>
+    <p>Calories: ${txt[i].recipe.calories} Kcal</p>
     `
-    foodContainerHtml +=` 
+    foodContainerHtml +=`
     <div class="ingredients-and-steps">
     <p class="center-text">Ingredients:<p>
     <ul>
     `
-    
-    for(var j = 0; j < txt[i].recipe.ingredientLines.length; j++) {
-        foodContainerHtml += `
-        <li>${txt[i].recipe.ingredientLines[j]}</li>      
-        `
-    }
-
+​
+    //Referencing the index of the ingredients themself
+        for(var j = 0; j < txt[i].recipe.ingredientLines.length; j++) {
+            foodContainerHtml += `
+            <li>${txt[i].recipe.ingredientLines[j]}</li>    
+            `
+        }
     
     foodContainerHtml += `
-    </ul>
+    </ul> 
     </div>
     </div>
     `
-
+    //Might need url from recipe's cuz that gives a link to the recipe
     console.log(foodContainerHtml);
     var html = foodContainerHtml;
     showinfoEL.innerHTML = html;
  }
 }
-
+​
+​
 formEL.addEventListener('submit', subButton);
--- assets/entrees.js
+++ assets/entrees.js
@@ -2,24 +2,26 @@ var formEL = document.querySelector (".pure-form");
 var infoEL = document.querySelector ("#info");
 var showtextEL =document.querySelector (".show");
 var showinfoEL = document.querySelector (".show-info");
-
+var choicesEl = document.querySelector(".choices");
+​
+​
 var subButton = function(event) {
     event.preventDefault();
-
+​
     var infoInput = infoEL.value.trim();
     if(infoInput) {
         displayInfo(infoInput);
     } else {
         showinfoEL.innerHTML = "Nothing was Found"
     }
-
+​
 }
-
+​
 function displayInfo(search) {
-    var url = "https://api.edamam.com/api/recipes/v2?type=public&q="+search+"&app_id=730b99e5&app_key=2eacc20905ac41a9a0d49163a5a68fec&dishType=Main%20course&dishType=Pancake&dishType=Salad&dishType=Sandwiches&dishType=Soup"
-    // var url = "https://api.edamam.com/api/recipes/v2?type=public" +search+ "&app_id=730b99e5&app_key=2eacc20905ac41a9a0d49163a5a68fec"
-    console.log(url);
-    fetch(url) 
+    var entreesUrl = "https://api.edamam.com/api/recipes/v2?type=public&q="+search+"&app_id=730b99e5&app_key=2eacc20905ac41a9a0d49163a5a68fec&dishType=Main%20course&dishType=Pancake&dishType=Salad&dishType=Sandwiches&dishType=Soup"
+​       
+    console.log(entreesUrl);
+    fetch(entreesUrl) 
     .then(function(response){
         if(response.ok) {
             response.json().then(function(data) {
@@ -28,6 +30,7 @@ function displayInfo(search) {
                     showinfoEL.innerHTML = "Nothing was Found";
                     return;
                 }
+​
                 displayTxt(data.hits);
                 console.log(data.hits);
                 console.log("General Data" + data);
@@ -35,41 +38,43 @@ function displayInfo(search) {
         }
     });
 }
-
+​
+​
+//Function to loop through the recipe array and display the individual stuff
 function displayTxt(txt) {
     var foodContainerHtml = "";
-    for(var i = 0; i < txt.length; i++) {
-       foodContainerHtml += `
-       <div class="food-container food-${i}">
-       <img src= "${txt[i].recipe.image}">
-       <p>${txt[i].recipe.label}</p>
-       <p>Description<p>
-       <p>Description Text<p>
-       <p>Calories: ${txt[i].recipe.calories}Kcal</p>
-       `
-       foodContainerHtml +=` 
-       <div class="ingredients-and-steps">
-       <p class="center-text">Ingredients:<p>
-       <ul>
-       `
-       
-       for(var j = 0; j < txt[i].recipe.ingredientLines.length; j++) {
-           foodContainerHtml += `
-           <li>${txt[i].recipe.ingredientLines[j]}</li>      
-           `
-       }
-   
-       
-       foodContainerHtml += `
-       </ul>
-       </div>
-       </div>
-       `
-   
-       console.log(foodContainerHtml);
-       var html = foodContainerHtml;
-       showinfoEL.innerHTML = html; 
+//You could add a line of code here to store calories and make it a whole number then represent it below
+ for(var i = 0; i < txt.length; i++) {
+    foodContainerHtml += `
+    <div class="food-container food-${i}">
+    <img src= "${txt[i].recipe.image}">
+    <p>${txt[i].recipe.label}</p>
+    <p>Calories: ${txt[i].recipe.calories} Kcal</p>
+    `
+    foodContainerHtml +=`
+    <div class="ingredients-and-steps">
+    <p class="center-text">Ingredients:<p>
+    <ul>
+    `
+​
+    //Referencing the index of the ingredients themself
+        for(var j = 0; j < txt[i].recipe.ingredientLines.length; j++) {
+            foodContainerHtml += `
+            <li>${txt[i].recipe.ingredientLines[j]}</li>    
+            `
+        }
+    
+    foodContainerHtml += `
+    </ul> 
+    </div>
+    </div>
+    `
+    //Might need url from recipe's cuz that gives a link to the recipe
+    console.log(foodContainerHtml);
+    var html = foodContainerHtml;
+    showinfoEL.innerHTML = html;
  }
 }
-
+​
+​
 formEL.addEventListener('submit', subButton);
